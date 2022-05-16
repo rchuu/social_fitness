a680ad2e74d177dba68fc351f360006be94edee0
Richard Chu <rchu1211@gmail.com>
2022-05-16 14:50:55 -0700

    initial commit

---
+++ flask_app/controllers/users.py
@@ -0,0 +1,44 @@
+from flask import render_template, redirect, request, session, flash
+from flask_app import app
+from flask_app.models.user import User
+# from flask_app.models.workout import Workout
+from flask_bcrypt import Bcrypt
+bcrypt = Bcrypt(app)
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/register', methods=['POST'])
+def register():
+    if not User.validate_register(request.form):
+        return redirect('/')
+
+    data = {
+        "first_name": request.form['first_name'],
+        "last_name": request.form['last_name'],
+        "email": request.form["email"],
+        "password": bcrypt.generate_password_hash(request.form['password'])
+    }
+    id = User.save(data)
+    session['user_id'] = id
+
+    return redirect('/dashboard')
+
+
+@app.route('/dashboard')
+def dashboard():
+    if 'user_id' not in session:
+        return redirect('/logout')
+    data = {
+        'id': session['user_id']
+    }
+    return render_template('dashboard.html', user=User.get_from_id(data))
+
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect('/')
